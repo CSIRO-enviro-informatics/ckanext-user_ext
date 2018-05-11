839f872c53a8ae8b7cd916224adf12f5319bac1e
Ashley Sommer <ashleysommer@gmail.com>
2018-05-11 10:04:25 +1000

    Fix for when the the plugin has configure() called more than once during application startup, causing the Table schema to be created twice, and later, the mapper to be mapped twice, causing two different halting errors. This can happen because _another_ plugin calls load_config() to reload the environment (looking at you, spatial plugins), but it also happens in CKAN 2.8.0 because it reloads the environment _twice_ during application startup now. With CKAN 2.8.0 _and_ Spatial plugins, configure() is called three times! This change adds much more error checking to detect and report problems, and also adds known configured mechanism to determine whether it needs to recreate the table schema or not during configuration.

--- ckanext/user_ext/model/__init__.py
+++ ckanext/user_ext/model/__init__.py
@@ -1,15 +1,15 @@
 import logging
 
-from sqlalchemy.exc import NoSuchTableError
-import migrate
-from migrate.versioning import schemadiff, genmodel
 log = logging.getLogger(__name__)
 import ckan.model
 from sqlalchemy import Table, Column, Boolean, UnicodeText, ForeignKey
-from sqlalchemy.orm import mapper
+from sqlalchemy.orm import mapper, class_mapper
+from sqlalchemy.exc import NoSuchTableError, InvalidRequestError
+from sqlalchemy.orm.exc import UnmappedClassError
+import migrate
+from migrate.versioning import schemadiff, genmodel
 
 table_name = 'user_ext'
-
 class UserExt(object):
     @classmethod
     def get(cls, user_id):
@@ -41,35 +41,56 @@ def get_table_diff(table):
     d = schemadiff.getDiffOfModelAgainstDatabase(metadata, metadata.bind, excludeTables=all_tables)
     return d
 
+
 def strip_diff(diff):
-    #ensure we are only getting
+    # ensure we are only getting the diff from _our_ table.
     diff.tables_missing_from_A = []
     diff.tables_missing_from_B = []
     if diff.tables_different:
         if table_name in diff.tables_different.keys():
-            diff.tables_different = {table_name: diff.tables_different[table_name]}
+            diff.tables_different =\
+                {table_name: diff.tables_different[table_name]}
         else:
             diff.tables_different = {}
     return diff
+
 
 def make_table(metadata, *cols):
     primary_col = Column('user_id', UnicodeText, ForeignKey("user.id"),
                          primary_key=True)
     auto_load = False
     create = False
+    extend = False
     try:
-        #test table exists?
-        _ = Table(table_name, metadata,
-                  primary_col, autoload=True)
+        # test table exists?
+        # setting keep_existing to False here causes the second exception
+        # to throw, indicating that the table is already in the metadata.
+        _ = Table(table_name, metadata, primary_col,
+                  autoload=True, keep_existing=False)
         auto_load = True
+        extend = True
     except NoSuchTableError:
         # don't autoload existing table
+        # don't extend existing table
         create = True
+    except InvalidRequestError:
+        # don't create new table
+        auto_load = True
+        extend = True
+    except Exception as e:
+        log.error(str(e))
+        raise e
 
-    user_ext = Table(table_name, metadata,
-                     primary_col, *cols,
-                     extend_existing=True,
-                     autoload=auto_load)
+    try:
+        user_ext = Table(table_name, metadata,
+                         primary_col, *cols,
+                         extend_existing=extend,
+                         mustexist=(not create),
+                         autoload=auto_load)
+    except Exception as e:
+        log.error(str(e))
+        raise e
+
     if create:
         metadata.create_all(bind=metadata.bind, tables=[user_ext])
     else:
@@ -77,8 +98,20 @@ def make_table(metadata, *cols):
         if d:
             g = genmodel.ModelGenerator(d, metadata.bind)
             g.runB2A()
-    mapper(UserExt, user_ext)
+
+    try:
+        _map = class_mapper(UserExt)
+        assert _map.class_ == UserExt
+        assert _map.local_table is not None
+        assert _map.local_table.name == table_name
+    except UnmappedClassError:
+        _map = mapper(UserExt, user_ext)
+    except AssertionError as e:
+        log.error(
+            "New mapping on UserExt didn't match existing mapping")
+        log.error(str(e))
     return user_ext
+
 
 def create_or_extend_table(*cols):
     if ckan.model.user_table.exists():
@@ -87,7 +120,26 @@ def create_or_extend_table(*cols):
     else:
         return None
 
+
 def register_additional_class(table, new_cls):
+    assert new_cls != UserExt
     assert issubclass(new_cls, UserExt)
-    mapper(new_cls, table, inherits=UserExt)
+
+    try:
+        _map = class_mapper(new_cls)
+        assert _map.class_ == new_cls
+        assert _map.local_table is not None
+        assert _map.local_table.name == table.name
+        assert _map.inherits
+        try:
+            i_class = _map.inherits.class_
+        except AttributeError:
+            i_class = _map.inherits
+        assert i_class == UserExt
+    except UnmappedClassError:
+        _map = mapper(new_cls, table, inherits=UserExt)
+    except AssertionError as e:
+        log.error(
+            "New mapping on existing class didn't match existing mapping")
+        log.error(str(e))
 
--- ckanext/user_ext/plugin.py
+++ ckanext/user_ext/plugin.py
@@ -40,8 +40,8 @@ class UserExtPlugin(SingletonPlugin):
 
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(**kwargs)
-        self.config = {}
         self.table = None
+        self.configured_cols = None
 
     #Configurer
     def update_config(self, config):
@@ -50,17 +50,21 @@ class UserExtPlugin(SingletonPlugin):
 
     #Configurable
     def configure(self, config):
-        self.config = config
         iuserextensions = PluginImplementations(IUserExtension)
         iuserextensions = iuserextensions.extensions()
         cols = []
+        these_configured_cols = set()
         for ext in iuserextensions:
+            ext_class = ext.__class__
             new_cols = ext.get_table_columns()
             if new_cols is None or len(new_cols) < 1:
                 continue
             for nc in new_cols:
                 assert isinstance(nc, Column)
                 col_name = nc.name
+                col_type = nc.type
+                col_c_type = col_type.__class__ if col_type else None
+                these_configured_cols.add((ext_class, col_name, col_c_type))
                 if nc.primary_key:
                     raise RuntimeError(
                         "Column {} cannot be a primary key.".format(col_name))
@@ -68,10 +72,16 @@ class UserExtPlugin(SingletonPlugin):
                     raise RuntimeError(
                         "Column {} cannot be a system col.".format(col_name))
                 cols.append(nc)
+        if self.configured_cols and \
+            len(these_configured_cols.difference(self.configured_cols)) == 0:
+            # we've configured already, and cols are still the same.
+            return
+        self.configured_cols = these_configured_cols
         try:
             self.table = create_or_extend_table(*cols)
         except Exception, e:
             log.error(str(e))
+            raise e
         if self.table is not None:
             reg = partial(register_additional_class, self.table)
             for ext in iuserextensions:
